e194860c87db355d2311dfb05644261e5d3f3d75
xing haha <xinghaha@xings-MacBook-Pro.local>
2019-02-26 18:04:51 +0700

    .1

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -10,20 +10,47 @@ void swap(T d[],int x,int y){
 }
 
 template <typename T>
-void insertionSort(T d[],int N){
-	
-//Write your code here.
-
+void insertionSort(T arr[],int N){
+    
+    int q;
+    for(int i = 1; i < 10; i++)
+    {
+        for(int k = 0;k < N;k++){
+            if(k == i) cout << " ["<<arr[k]<<"]";
+            else
+            {
+                cout << " " << arr[k];
+            }   
+        }
+        cout << " => ";
+        for(int a = i-1; a>=0 ; a--)
+        {
+        if (arr[a] < arr[a+1]) {
+            swap(arr ,a,a+1);
+            q=a;
+        }
+        }
+        for(int k = 0;k < N;k++){
+            if(k == q) cout << " ["<<arr[k]<<"]";
+            else
+            {
+                cout << " " << arr[k];
+            }       
+        }
+        cout << endl;
+        
+    }
+cout<<"\n";
 }
-
 int main(){
-	int a[10] = {12,25,30,44,2,0,4,7,55,25};
-	cout << "Input Array:";
-	for(int i = 0; i < 10; i++) cout << a[i] << " ";
-	cout << "\n\n";
-	
-	insertionSort(a,10);
-	
-	cout << "\nSorted Array:";
-	for(int i = 0; i < 10; i++) cout << a[i] << " ";	
+    int a[10] = {12,25,30,44,2,0,4,7,55,25};
+    cout << "Input Array:";
+    for(int i = 0; i < 10; i++) cout << a[i] << " ";
+    cout << "\n\n";
+    
+    insertionSort(a,10);
+    
+    cout << "\nSorted Array:";
+    for(int i = 0; i < 10; i++) cout << a[i] << " ";    
 }
+
